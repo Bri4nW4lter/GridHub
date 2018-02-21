b6b6b01f46d122472416759c3249b61bdca0a454
Raekar <marcrichter-stade@web.de>
2018-02-21 10:58:30 +0100

    fixed character controls

---
+++ Assets/Scripts/Player_Network.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class Player_Network : NetworkBehaviour
+{
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<ThirdPersonCharacter>().enabled = true;
+
+
+    }
+}
